e88f123cc04b5e4d9b8baf7e7080250b7b8dec52
HanJiangtao <h15968804551@foxmail.com>
2022-02-20 22:21:50 +0800

    dynamic symbol extern c, do not with dynamic symbol

--- cpp_workspace/dynamic_learning/dynamic.h
+++ cpp_workspace/dynamic_learning/dynamic.h
@@ -6,5 +6,19 @@
 
 #ifndef __DYNAMIC_H__
 #define __DYNAMIC_H__
+
+////////////////////////////
+// method to output symbol
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int qq();
+
+#ifdef __cplusplus
+}
+#endif
+// extern c end
+////////////////////////////
+
 #endif
