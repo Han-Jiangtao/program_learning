1a6933d21b54bf36b4d98474c4107dc1b0fc09c4
HanJiangtao <hanjiangtao-pub@qq.com>
2022-04-23 11:35:30 +0800

    proto test begin

--- cpp_workspace/cpp_learning.cpp
+++ cpp_workspace/cpp_learning.cpp
@@ -7,11 +7,23 @@
 
 #include "common/log.h"
 #include "common/utils/string_util.h"
-#include "learning.pb.h"
+#include "protobuf_learning.pb.h"
+
+void proto_test()
+{
+    protobuf_learning::simple_data a;
+    void *b = malloc(1024 * 1024 * 1024);
+    memset(b, 1, 1024 * 1024 * 1024);
+    a.set_data(b, 1024 * 1024 * 1024);
+}
 
 int MAIN(int argc, char* argv[]) {
     COMMON::LogUtil logUtil = COMMON::LogUtil(argv[0]);
     for (int argLoc = 0; argLoc < argc; argLoc++) {
         LOG(INFO) << UTILS::StrUtil::StrFormat("arg[%d]:%s", argLoc, argv[argLoc]);
+    }
+    for (int i = 0; i < 1000; i++) {
+        LOG(INFO) << UTILS::StrUtil::StrFormat("proto_test[%d]", i);
+        proto_test();
     }
 }
