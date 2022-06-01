378119d9bb59af617d681f458346bcd57d22a047
HanJiangtao <h15948804551@foxmail.com>
2022-06-01 11:08:50 +0800

    refresh test

--- python_workspace/mp_resource_manager.py
+++ python_workspace/mp_resource_manager.py
@@ -6,18 +6,20 @@ import multiprocessing
 import atexit
 from multiprocessing.util import Finalize
 
-
 class GlobalMgr(threading.Thread):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.mgr = multiprocessing.Manager()
         self.task_q = self.mgr.Queue()
+        self.q = self.mgr.Queue()
         self.res_d = self.mgr.dict()
         self.resource_l = self.mgr.Lock()
         self.stop_event = threading.Event()
 
     def run(self):
         while not self.stop_event.is_set():
+            #time.sleep(0.001)
+            #continue
             try:
                 key, func, args, kwargs = self.task_q.get(timeout=0.001)
                 self.resource_l.acquire()
@@ -25,6 +27,8 @@ class GlobalMgr(threading.Thread):
                 self.resource_l.release()
             except queue.Empty as e:
                 continue
+    def __del__(self, *args, **kwargs):
+        print("hanjiangtao __del__")
 
     def lock(self, *args, **kwargs):
         return self.mgr.Lock(*args, **kwargs)
@@ -32,23 +36,15 @@ class GlobalMgr(threading.Thread):
     def event(self, *args, **kwargs):
         return self.mgr.Event(*args, *kwargs)
 
-def get_resource_from_global_mgr(tools, func, *args, **kwargs):
-    key = str(time.time())
-    tools["task_q"].put((key, func, args, kwargs))
-    res = None
-    while res is None:
-        tools["resource_l"].acquire()
-        res = tools["res_d"].pop(key, None)
-        tools["resource_l"].release()
-        time.sleep(0.0001)
-    return res
 
-def test_resource(tools):
-    event = get_resource_from_global_mgr(tools, "event")
-    lock = get_resource_from_global_mgr(tools, "lock")
+def test_resource(event, lock, q):
     print("hanjiangtao start!", event, lock, flush=True)
     cnt = 0
     while not event.is_set():
+        try:
+            a = q.get(timeout=0.001)
+        except queue.Empty as e:
+            continue
         lock.acquire()
         time.sleep(0.01)
         print(cnt, flush=True)
@@ -56,7 +52,7 @@ def test_resource(tools):
         lock.release()
 
 def release(mgr, process):
-    print("hanjiangtao register release")
+    print("hanjiangtao register release", flush=True)
     mgr.stop_event.set()
     mgr.join()
     process.terminate()
@@ -66,16 +62,12 @@ def release(mgr, process):
 def subprocess():
     mgr = GlobalMgr(daemon=True)
     mgr.start()
-    tools = {
-        "resource_l": mgr.resource_l,
-        "res_d": mgr.res_d,
-        "task_q": mgr.task_q
-    }
-    process = multiprocessing.Process(target=test_resource, args=(tools,), daemon=True)
+    process = multiprocessing.Process(target=test_resource,
+        args=(mgr.stop_event, mgr.resource_l, mgr.q,), daemon=True)
     process.start()
     #Finalize(None, release, args=(mgr, process,), exitpriority=100)
-    atexit.register(release, mgr, process)
-    time.sleep(2)
+    #atexit.register(release, mgr, process)
+    time.sleep(1)
 
 if __name__ == "__main__":
     process = multiprocessing.Process(target=subprocess, daemon=False)
