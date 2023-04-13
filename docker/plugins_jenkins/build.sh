fddc87345fe83949a0b791f4d2616d06e4a56c33
HanJiangtao <h15968804551@foxmail.com>
2023-04-13 19:53:32 +0800

    ignore DS_store

--- docker/plugins_jenkins/build.sh
+++ docker/plugins_jenkins/build.sh
@@ -1,6 +1,16 @@
 #/bin/bash
 version=$1
+if [ -z "$version" ]
+then
+    echo "The script need one param(version). comman:bash build.sh version"
+    exit 127
+fi
 image_name="jenkins/plugins_amd64:$version"
-file_name="jenkins_plugins_amd64_$version"
+file_name="jenkins_plugins_amd64_$version.tar"
+echo "We will build docker image:$image_name! Cost about 200s!"
 docker buildx build --no-cache -t $image_name --platform linux/amd64 .
+echo "Docker build[$?], shell command[docker images] can find image named $image_name!"
+echo "Save image[$image_name] to file[$file_name]!"
+docker save -o $file_name $image_name
+echo "All step end!"
 
