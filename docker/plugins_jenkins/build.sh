79ea93c97b67dff4ac1b49b141b1fd903d142029
hanjiangtao_macbookair <hanjiangtao-pub@hotmail.com>
2024-06-21 11:14:38 +0800

    add post build

--- docker/plugins_jenkins/build.sh
+++ docker/plugins_jenkins/build.sh
@@ -5,6 +5,10 @@ then
     echo "The script need one param(version). comman:bash build.sh version"
     exit 127
 fi
+
+docker rmi jenkins/jenkins:jdk17
+docker pull jenkins/jenkins:jdk17
+
 image_name="jenkins/plugins_amd64:$version"
 file_name="jenkins_plugins_amd64_$version.tar"
 echo "We will build docker image:$image_name! Cost about 200s!"
@@ -12,5 +16,7 @@ docker buildx build --no-cache -t $image_name --platform linux/amd64 .
 echo "Docker build[$?], shell command[docker images] can find image named $image_name!"
 echo "Save image[$image_name] to file[$file_name]!"
 docker save -o $file_name $image_name
+
+docker rmi jenkins/jenkins:jdk17
 echo "All step end!"
 
